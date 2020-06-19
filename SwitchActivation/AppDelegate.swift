5b6c19e232e7cb91e26fa5f712578c00c08e2bd5
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-19 00:20:05 -0700

    Initial Commit

---
+++ SwitchActivation/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwitchActivation
+//
+//  Created by Sparta Science on 6/19/20.
+//  Copyright © 2020 Sparta Science. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwitchActivation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwitchActivation
+//
+//  Created by Sparta Science on 6/19/20.
+//  Copyright © 2020 Sparta Science. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
