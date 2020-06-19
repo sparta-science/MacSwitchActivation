38a4c5e98ec89423a2cc091723bc322db78b8840
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-19 00:34:27 -0700

    add window controller

---
+++ SwitchActivation/WindowController.swift
@@ -0,0 +1,16 @@
+//
+//  WindowController.swift
+//  SwitchActivation
+//
+//  Created by Sparta Science on 6/19/20.
+//  Copyright © 2020 Sparta Science. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        print(#function)
+    }
+}
