ad3c5fa6e2407c78fd9d84bf97258930dbb1c607
Ben Summers <benlsummers@gmail.com>
2020-06-20 00:47:42 -0700

    assert window and app states on activation

--- SwitchActivation/WindowController.swift
+++ SwitchActivation/WindowController.swift
@@ -13,15 +13,22 @@ class WindowController: NSWindowController {
     override func windowDidLoad() {
         super.windowDidLoad()
         print(#function)
+        assert(NSApp.mainWindow == nil, "loaded before becoming main")
     }
     @IBAction func showMainWindow(_ sender: Any) {
         showWindow(sender)
         NSApp.setActivationPolicy(.regular)
         defaults.set(false, forKey: "accessory")
-        NSApp.activate(ignoringOtherApps: true)
-        window?.becomeMain()
-        window?.becomeKey()
-        window?.menu = NSApp.mainMenu
+        let running = NSRunningApplication.current
+        assert(!running.isHidden)
+        assert(running.isActive)
+        assert(running.ownsMenuBar)
+        assert(NSApp.mainMenu != nil)
+        assert(window != nil)
+        assert(window!.isVisible)
+        DispatchQueue.main.async {
+            assert(NSApp.mainWindow === self.window, "should become main")
+        }
     }
     
     override func showWindow(_ sender: Any?) {
