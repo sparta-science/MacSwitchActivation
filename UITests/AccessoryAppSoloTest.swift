ef65d35aa55d1f4545c5fe734dba0957cdfa48c7
Paul Zabelin <paulz@users.noreply.github.com>
2020-06-20 22:34:36 -0700

    experiment with log running applications

--- UITests/AccessoryAppSoloTest.swift
+++ UITests/AccessoryAppSoloTest.swift
@@ -2,12 +2,9 @@ import XCTest
 
 class AccessoryAppSoloTest: XCTestCase {
     let app = XCUIApplication()
-    lazy var processId = app.value(forKeyPath: "_applicationImpl._currentProcess._processID") as! Int
     lazy var menuBarsQuery = app.menuBars
     lazy var mainMenu = menuBarsQuery["main menu"]
     lazy var menuBarStatusItem = menuBarsQuery.statusItems["home"]
-    var failedAsExpected = false
-    var expectedFailureAtLine: Int?
 
     func testAccessingMenuCauseFailure() throws {
         app.launchArguments = ["-startAsAccessory", "YES"]
@@ -16,24 +13,9 @@ class AccessoryAppSoloTest: XCTestCase {
                       "should be running in background, was: \(app.state.rawValue)")
         XCTAssertEqual(app.windows.count, 0, "should not show window")
         showMainWindowAndInteract()
-        XCTAssertTrue(mainMenu.exists)
-        let appMenuItem = mainMenu.menuBarItems["SwitchActivation"]
-        XCTAssertTrue(appMenuItem.exists)
-        XCTAssertEqual(appMenuItem.identifier, "app menu item")
-        expectedFailureAtLine = #line; _ = appMenuItem.isHittable
-        addTeardownBlock {
-            XCTAssertTrue(self.failedAsExpected)
-        }
+        verifyMenuWorks()
     }
     
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
-
-    override func tearDownWithError() throws {
-        app.terminate()
-    }
-
     func showMainWindowAndInteract() {
         menuBarStatusItem.click()
         app.menuItems["Show Window"].click()
@@ -44,22 +26,31 @@ class AccessoryAppSoloTest: XCTestCase {
         textField.typeKey("a", modifierFlags:.command)
         textField.typeKey("c", modifierFlags:.command)
     }
-    
-    func verifyExpectedFailure(withDescription description: String) {
-        XCTAssertTrue(description.hasPrefix("Failed to hit test MenuBarItem, {{45.0, 0.0}, {136.0, 22.0}}, "
-            + "identifier: 'app menu item', title: 'SwitchActivation', label: 'first app menu item' at point {"))
-        XCTAssertTrue(description.contains("}: found AX element pid: \(processId), token:"))
-        XCTAssertTrue(description.hasSuffix(" which is from the same process but cannot be mapped using AX data: Failed to find parent of element Application, pid: \(processId), title: \'SwitchActivation\', Disabled, lookup returned nil or invalid object (null)"))
-        NSLog("expected failure: \(description)")
+
+    func verifyMenuWorks() {
+        mainMenu.menuBarItems["SwitchActivation"].click()
+        mainMenu.menus.menuItems["About SwitchActivation"].click()
+        XCTAssertTrue(app.staticTexts["Version 1.0 (1)"].waitForExistence(timeout: 1))
     }
     
-    override func recordFailure(withDescription description: String, inFile filePath: String, atLine lineNumber: Int, expected: Bool) {
-        if lineNumber == expectedFailureAtLine, filePath == #file, expected {
-            verifyExpectedFailure(withDescription: description)
-            failedAsExpected = true
-        } else {
-            super.recordFailure(withDescription: description, inFile: filePath, atLine: lineNumber, expected: true)
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        NSWorkspace.shared.runningApplications.forEach {
+            let property: [Any] = [
+                $0.localizedName!,
+                $0.isActive,
+                $0.isHidden,
+                $0.activationPolicy.rawValue,
+                $0.bundleIdentifier ?? "no bundle id",
+                $0.bundleURL!.path,
+                $0.executableURL!.path,
+            ]
+            NSLog(property.debugDescription)
         }
     }
-   
+
+    override func tearDownWithError() throws {
+        app.terminate()
+    }
+
 }
